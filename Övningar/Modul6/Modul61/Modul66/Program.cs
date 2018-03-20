2fc8905147bd1716ec5872cc4ec69cb60abbbe8c
Anders Dahl <anders.dahl@collectorbank.se>
2018-03-20 16:36:05 +0100

    Har endast börjat me detta.

---
+++ Övningar/Modul6/Modul61/Modul66/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Modul66
+{
+    class Program
+    {
+        private static object Response;
+
+        static void Main(string[] args)
+
+
+        
+        {
+            if (Regex.IsMatch(zipcode, @"^\d{3}\s\d{2}$"))
+                Zipcode = zipcode
+                                else
+                Console.WriteLine();
+        }
+        public void SetZipcode(string zipcode)
+    }
+}
