bfd451446d1ba9bd352deea45817c3ee0dc35850
Anders Dahl <anders.dahl@collectorbank.se>
2018-03-20 16:13:36 +0100

    Klar med uppgiften

---
+++ Övningar/Modul6/Modul61/Modul65/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Modul65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adress hem = new Adress("Bergsgatan", 7, "Varberg", 12365);
+            Adress villan = new Adress("Kungsgatan", 10, "Stockholm", 45672);
+            villan.GetFullStreet();
+        }
+    }
+    class Adress
+    {
+        public string Street { get; set; }
+        public int StreetNumber { get; set; }
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+
+        public Adress(string street, int streetnumber, string city, int zipcode)
+        {
+            Street = street;
+            StreetNumber = streetnumber;
+            City = city;
+            ZipCode = zipcode;
+
+
+        }
+        public void GetFullStreet()
+        {
+            Console.WriteLine($"The full adress is {Street} {StreetNumber} {City} {ZipCode}");
+        }
+
+
+    }
+}
