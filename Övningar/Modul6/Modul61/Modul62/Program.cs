ac980cdf742a59236d16be258e2a88c75e5f4aef
Anders Dahl <anders.dahl@collectorbank.se>
2018-03-20 10:42:07 +0100

    Skapades som nya

---
+++ Övningar/Modul6/Modul61/Modul62/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Modul61
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Cube mycube = new Cube(2, 3, 4);
+            Cube supercube = new Cube(100, 200, 300);
+
+            mycube.WriteVolume();
+            supercube.WriteVolume();
+        }
+    }
+    class Cube
+    {
+        int _length;
+        int _height;
+        int _depth;
+
+        public Cube(int height, int length, int depth)
+        {
+            _length = length;
+            _height = height;
+            _depth = depth;
+
+
+        }
+
+        public void mycube()
+        {
+        
+
+        }
+        public void mysupercube()
+        {
+
+
+
+        }
+        public void WriteVolume()
+        {
+            double volume = _length * _height * _depth;
+            Console.WriteLine($"The volume of the cube is {volume}");
+           
+
+
+        }
+    }
+}
