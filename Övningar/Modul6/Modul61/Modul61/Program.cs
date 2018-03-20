2fd13d177a6ca0a90730a6e43038d46e09d7407a
Anders Dahl <anders.dahl@collectorbank.se>
2018-03-20 10:14:38 +0100

    Modul 6. Skapade Modul 61.

---
+++ Övningar/Modul6/Modul61/Modul61/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Modul61
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Circle bob = new Circle("Bob", 8);
+            Circle lisa = new Circle("Lisa", 30);
+
+            bob.SayHello();
+            lisa.SayHello();
+
+            Console.WriteLine();
+
+            bob.WriteArea();
+            lisa.WriteArea();
+        }
+    }
+    class Circle
+    {
+        string _name;
+        float _radius;
+
+        public Circle(string name, int radius)
+        {
+            _name = name;
+            _radius = radius;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"I am a circle with the name of {_name}");
+
+
+        }
+        public void WriteArea()
+        {
+            double area = Math.Pow(_radius, 2) * Math.PI;
+            Console.WriteLine($"My name is {_name}.I have the radius of {_radius} and an area of {area}");
+        }
+    }
+}
