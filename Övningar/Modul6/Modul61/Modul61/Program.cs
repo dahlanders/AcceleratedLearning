2d8ca942ebc78ddb13f1c0768a13e49fd7022ee9
Anders Dahl <anders.dahl@collectorbank.se>
2018-03-20 10:41:46 +0100

    Jag la till Ali i programmet

--- Övningar/Modul6/Modul61/Modul61/Program.cs
+++ Övningar/Modul6/Modul61/Modul61/Program.cs
@@ -8,14 +8,17 @@ namespace Modul61
         {
             Circle bob = new Circle("Bob", 8);
             Circle lisa = new Circle("Lisa", 30);
+            Circle ali = new Circle("Ali", 5);
 
             bob.SayHello();
             lisa.SayHello();
+            ali.SayHello();
 
             Console.WriteLine();
 
             bob.WriteArea();
             lisa.WriteArea();
+            ali.WriteArea();
         }
     }
     class Circle
