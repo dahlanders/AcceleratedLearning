0b10ed610f33c6af354531b04aad4c9516214491
Anders Dahl <anders.dahl@collectorbank.se>
2018-03-21 09:29:31 +0100

    Blev klar med 3.7

---
+++ Övningar/Modul3/Modul3/Modul37/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Modul37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number as setvalue:");
+            int setnumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string classify;
+
+            //if (number > setnumber)
+                classify = "To high";
+            //else
+                classify = "Too low";
+
+            classify = (setnumber < number) ? "To high" :(setnumber == number) ?"Equal" : "Too low";
+
+            Console.WriteLine(classify);
+
+        }
+    }
+}
