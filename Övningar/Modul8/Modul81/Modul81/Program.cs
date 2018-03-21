27eb5ca5fddb0eb3ca75ed5ef32d771797acc3a3
Anders Dahl <anders.dahl@collectorbank.se>
2018-03-21 16:23:12 +0100

    Vi har jobbat med detta i dag 2018-03-21

---
+++ Övningar/Modul8/Modul81/Modul81/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Modul81
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pices = HowManyPices();
+
+            Console.Write("How many people are you?:");
+            decimal people = Convert.ToInt32(Console.ReadLine());
+
+            //decimal count = pices / people;
+            //Console.WriteLine("Pices per people" + count);
+
+            try
+            {
+                if (people == 0)
+                {
+                    throw new ArgumentException("People kan inte vara 0");
+                }
+                throw new Exception("Detta är en test");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Fel inpu");
+            }
+        }
+        static decimal HowManyPices()
+        {
+            Console.Write("How many pices do you want to share?:");
+            decimal pices = decimal.Parse(Console.ReadLine());
+            return pices;
+        }
+    }
+}
---
+++ Övningar/Modul8/Modul81/Modul82/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Modul82
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           File.Create()
+        }
+    }
+}
