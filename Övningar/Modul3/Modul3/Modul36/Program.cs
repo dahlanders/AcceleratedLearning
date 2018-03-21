c0b29516d6ca50a223396b1e6b08a2b592f1d154
Anders Dahl <anders.dahl@collectorbank.se>
2018-03-21 09:29:15 +0100

    Blev klar 3.6

---
+++ Övningar/Modul3/Modul3/Modul36/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Modul36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number as setvalue:");
+            int setnumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > setnumber)
+            {
+                Console.WriteLine($"It´s higher then {setnumber}");
+            }
+            else if(number == setnumber)
+            {
+                Console.WriteLine($"It´s equal to {setnumber}");
+            }
+            else
+            {
+                Console.WriteLine($"It´s lower then {setnumber}");
+            }
+        }
+    }
+}
