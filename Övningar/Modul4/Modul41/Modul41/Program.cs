94051762af032d8c753029b3dcd718016ca137dd
Anders Dahl <anders.dahl@collectorbank.se>
2018-03-21 09:30:25 +0100

    Skapade modul 4 och började med 4.1

---
+++ Övningar/Modul4/Modul41/Modul41/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Modul41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            string[] nameList = names.Split(",");
+
+            foreach (string name in nameList)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("***SUPER-" + name.ToUpper() + "***");
+                Console.ResetColor();
+            }
+
+
+        }
+        public void GetInputFromUser()
+        {
+            Console.Write("Enter names seperated by comma( e.g Maria, Peter,Lisa):");
+            string names = Console.ReadLine();
+            return names;
+
+        }
+    }
+}
